a75c488066bbf5ee9572b96a5eab558bd314e2fa
meharil <mehari.legesse@gmail.com>
2015-02-21 14:37:20 -0500

    test repo

---
+++ server/http_get_post.js
@@ -0,0 +1,66 @@
+var http = require('http');
+var url = require('url');
+var items = [];
+var server = http.createServer(function(req, res) {
+	if ('/' == req.url) {
+		switch (req.method) {
+			case 'POST':
+				add(req,res);
+				break;
+
+			case 'GET':
+				show(res);
+				break;
+
+			default:
+				badRequest(res);
+
+		}
+	} else {
+		notFound(res);
+	}
+	
+});
+
+function show(res) {
+	var html = '<h1>Todo List</h1>'
+				+ '<ul>'
+				+ items.map(function(item) {
+					return '<li>' + item + '</li>'
+				}).join('')
+				+ '</ul>'
+				+ '<form method="post" action="/">'
+				+ '<p><input type="text" name="item" /></p>'
+				+ '<p><input type="submit" value="Add item" /></p>'
+				+ '</form>';
+	res.setHeader('Content-Type', 'text/html');
+	res.setHeader('Content-Length', Buffer.byteLength(html));
+	res.end(html);
+}
+
+function notFound(res) {
+	res.statusCode = 404;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Not Found');
+}
+
+function badRequest(res) {
+	res.statusCode = 400;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Bad Request');
+}
+
+var qs = require('querystring');
+function add(req, res) {
+	var body = '';
+	req.setEncoding('utf8');
+	req.on('data', function(chunk) {
+		body += chunk
+	});
+	req.on('end', function() {
+		var obj = qs.parse(body);
+		items.push(obj.item);
+		show(res);
+	});
+}
+server.listen(3000);
