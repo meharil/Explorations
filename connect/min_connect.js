9aacf0978d72fb5305463d328b9ed3df3cd94ff6
meharil <mehari.legesse@gmail.com>
2015-03-07 12:26:13 -0500

    cn 1

--- connect/min_connect.js
+++ connect/min_connect.js
@@ -8,3 +8,8 @@ function logger(req, res, next) {
 	console.log('%s %s', req.method, req.url);
 	next();
 }
+
+function hello(req, res){
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('hello world');
+}
