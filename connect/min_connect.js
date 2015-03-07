b77161a0f12a7efaa74f1f55c39f5da601dd53ed
meharil <mehari.legesse@gmail.com>
2015-03-07 12:57:32 -0500

    cn 3

--- connect/min_connect.js
+++ connect/min_connect.js
@@ -1,8 +1,4 @@
 var connect = require('connect');
-var app = connect();
-app.use(logger);
-app.use(hello);
-app.listen(3000);
 
 
 function logger(req, res, next) {
@@ -14,3 +10,26 @@ function hello(req, res){
 	res.setHeader('Content-Type', 'text/plain');
 	res.end('hello world');
 }
+
+function restrict(req, res, next){
+	var authorization = req.headers.authorization;
+	if (!authorization) return next(new Error('Unauthorized'));
+	var parts = authorization.split(' ')
+	var scheme = parts[0]
+	var auth = new Buffer(parts[1], 'base64').toString().split(':')
+	var user = auth[0]
+	var pass = auth[1];
+
+	authenticateWithDB(user, pass, function(err) {
+
+		if (err) return next(err);
+		if (user == 'ml' && pass == 'ml') next();
+	});
+}
+
+connect()
+	.use(logger)
+	.use('/admin', restrict)
+	.use('/admin', admin)
+	.use(hello)
+	.listen(3000);
