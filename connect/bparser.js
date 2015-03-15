4afb85ddb03d363e88b77f815490f90932fe3fae
meharil <mehari.legesse@gmail.com>
2015-03-15 16:38:58 -0400

    bp3

--- connect/bparser.js
+++ connect/bparser.js
@@ -4,5 +4,6 @@ var bodyParser = require('body-parser');
 var app = connect()
 	.use(bodyParser.json())
 	.use(function(req, res) {
+		console.log("Registered new user: " + req.body.username);
 		res.end('Registered new user: ' + req.body.username);
 	}).listen(3000);
