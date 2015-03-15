69367da035f6fac410d9c34253d7de3cd66118cd
meharil <mehari.legesse@gmail.com>
2015-03-15 12:13:18 -0400

    ck3

--- connect/cookies.js
+++ connect/cookies.js
@@ -1,10 +1,17 @@
 var connect = require('connect');
 var cookieParser = require('cookie-parser');
 
+// var app = connect()
+// 	.use(cookieParser('tobi is a ferret'))
+// 	.use(function(req, res) {
+// 		console.log(req.cookies);
+// 		console.log(req.signedCookies);
+// 		res.end('hello\n');
+// 	}).listen(3000);
+
 var app = connect()
-	.use(cookieParser('tobi is a ferret'))
 	.use(function(req, res) {
-		console.log(req.cookies);
-		console.log(req.signedCookies);
-		res.end('hello\n');
+		res.setHeader('Set-Cookie', 'foo=bar');
+		res.setHeader('Set-Cookie', 'tobi=ferret; Expires=Tue, 08 Jun 2020 12:12:15 GMT');
+		res.end();
 	}).listen(3000);
