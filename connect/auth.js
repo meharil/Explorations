a1679d224e902622a409c651ba191e7595f54b94
meharil <mehari.legesse@gmail.com>
2015-03-15 21:16:46 -0400

    auth4

--- connect/auth.js
+++ connect/auth.js
@@ -1,7 +1,19 @@
 var connect = require('connect');
 var basicAuth = require('basic-auth');
 var app = connect()
-	.use(basicAuth('tobi', 'ferret'))
-	.use(function (req, res) {
-		res.end('I am a secret\n');
+	.use(function(req, res, next) {
+		var user = basicAuth(req);
+		if (!user || user.name !== 'tobi' || user.pass !== 'ferret'){
+			console.log('your in!');
+			res.statusCode = 401;
+			res.end('Unauthorized');
+		}else {
+			console.log('your in!');
+			next();
+		}
 	});
+	.use(function (req, res) {
+		res.end("I'm a secret\n");
+	});
+
+	app.listen(3000);
