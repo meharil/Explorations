f87240c0c6c1024bf0dc0ecd3d9a1d5535a01366
meharil <mehari.legesse@gmail.com>
2015-03-29 19:46:08 -0400

    bp 1

--- connect/bparser.js
+++ connect/bparser.js
@@ -1,9 +1,17 @@
 var connect = require('connect');
 var bodyParser = require('body-parser');
 
-var app = connect()
+/*var app = connect()
 	.use(bodyParser.json())
 	.use(function(req, res) {
 		console.log("Registered new user: " + req.body.username);
 		res.end('Registered new user: ' + req.body.username);
+	}).listen(3000);*/
+
+var app = connect()
+	.use(bodyParser.urlencoded())
+	.use(function(req, res) {
+		console.log(req.body);
+		console.log(req.files);
+		res.end('thanks!')
 	}).listen(3000);
