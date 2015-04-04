18777d2e51195157441ee5e1cbe2ac3984e3a22d
meharil <mehari.legesse@gmail.com>
2015-04-04 10:37:32 -0400

    log4

--- connect/cLogger.js
+++ connect/cLogger.js
@@ -1,8 +1,13 @@
 var connect = require('connect');
 var morgan = require('morgan');
+var url = require('url');
+
+morgan.token('query-string', function(req, res) {
+	return url.parse(req.url).query;
+})
 
 var app = connect()
-	.use(morgan(':method :url'))
+	.use(morgan(':query-string :response-time'))
 	.use(hello)
 	.listen(3000);
 
