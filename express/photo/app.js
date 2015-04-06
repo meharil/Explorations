1c56e57dc7275a8d3d3541d6b26cb61d12d8a366
meharil <mehari.legesse@gmail.com>
2015-04-05 21:36:49 -0400

    exp 3

--- express/photo/app.js
+++ express/photo/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var photos = require('./routes/photos');
 
 var app = express();
 
@@ -22,7 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+//app.use('/', routes);
+app.use('/', photos.list);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
