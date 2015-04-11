856abff5de6f2f502f145ab3c37323677915b177
meharil <mehari.legesse@gmail.com>
2015-04-11 12:56:48 -0400

    exp6

--- express/photo/app.js
+++ express/photo/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var multer = require('multer');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +22,7 @@ app.set('photos', path.join(__dirname, 'public/photos'));
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
+app.use(multer());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
