1ba7e378d6016db94733617ca49cc6ea41a20e9c
meharil <mehari.legesse@gmail.com>
2015-04-13 08:52:37 -0400

    exp 15

--- express/photo/models/Photo.js
+++ express/photo/models/Photo.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-console.log('db url', db_string);
+console.log('db url', app.locals.db_string);
 mongoose.connect('mongodb://meharil:Shady_1@ds039010.mongolab.com:39010/node-db');
 var schema = new mongoose.Schema({
 	name: String,
