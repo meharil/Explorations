67e280775f97e12ce9677291e0c7cc96bee0dc3d
meharil <mehari.legesse@gmail.com>
2015-04-12 20:31:12 -0400

    exp 9

--- express/photo/routes/photos.js
+++ express/photo/routes/photos.js
@@ -44,7 +44,7 @@ exports.form = function(req, res){
 exports.submit = function (dir) {
 	return function(req, res, next){
 		console.log("req files", req.files, req.files['photo[image]']);
-		var img = req.files.photo.image;
+		var img = req.files['photo[image]'];
 		var name = req.body.photo.name || img.name;
 		var path = join(dir, img.name);
 		fs.rename(img.path, path, function(err) {
