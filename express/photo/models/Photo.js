58b7f60ff3cc99850d8c316c88b446b8e8dd6ac6
meharil <mehari.legesse@gmail.com>
2015-04-14 20:50:34 -0400

    exp 15

--- express/photo/models/Photo.js
+++ express/photo/models/Photo.js
@@ -1,8 +1,14 @@
 var mongoose = require('mongoose');
-var app = require('../app');
+var fs = require('fs');
 
-console.log('db url', app.locals.db_string);
-mongoose.connect('mongodb://meharil:Shady_1@ds039010.mongolab.com:39010/node-db');
+var stream = fs.createReadStream('../public/settings/setting');
+stream.on('data', function(chunk){
+	var setting = JSON.parse(chunk);
+	mongoose.connect(setting.replace(/(\:\/\/)/, '://'+obj.user+':'+obj.password+'@'));
+
+});
+
+//mongoose.connect('mongodb://meharil:Shady_1@ds039010.mongolab.com:39010/node-db');
 var schema = new mongoose.Schema({
 	name: String,
 	path: String
