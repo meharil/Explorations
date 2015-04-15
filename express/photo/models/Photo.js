f0f309c4590bbe89748a9c711fe8cf73c6ea5dfe
meharil <mehari.legesse@gmail.com>
2015-04-14 20:54:48 -0400

    exp 19

--- express/photo/models/Photo.js
+++ express/photo/models/Photo.js
@@ -7,6 +7,9 @@ stream.on('data', function(chunk){
 	mongoose.connect(setting.replace(/(\:\/\/)/, '://'+obj.user+':'+obj.password+'@'));
 
 });
+stream.on('error', function(err) {
+		process.stderr.write("ERROR: " + err.message + "\n");
+});
 
 //mongoose.connect('mongodb://meharil:Shady_1@ds039010.mongolab.com:39010/node-db');
 var schema = new mongoose.Schema({
