9e53bd402c3db2af9f391a7d258b04ced47d1a37
meharil <mehari.legesse@gmail.com>
2015-04-19 21:43:47 -0400

    template 4

--- templating/sample1.js
+++ templating/sample1.js
@@ -2,4 +2,4 @@ var ejs = require('ejs');
 var template = '<%= message %>';
 var context = {message: 'Hello template!'};
 
-console.log(ejs.render(template, {locals: context}));
+console.log(ejs.render(template, context));
